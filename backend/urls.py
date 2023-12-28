7e675d2797906e395355c140f028226682ee6096
k0luchiy <antoshka.osipov.04@mail.ru>
2023-12-28 20:33:10 +0500

    Static url

--- backend/urls.py
+++ backend/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('', include("folders.urls")),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
